1e22992d50afffadf5837c3b0242f87a9f6345cc
unknown <anonvtrede@gmail.com>
2021-11-12 18:36:30 +0300

    created CRUD (99 LESSON)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,14 @@ end
 
 def edit
   @article = Article.find(params[:id])
- 
 end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  redirect_to articles_path
+end
+
 def update  
   @article = Article.find(params[:id])
   if @article.update(params.require(:article).permit(:title, :description))
