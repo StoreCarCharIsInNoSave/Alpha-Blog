c91812a6e2ee708758007a738090ee436567d015
unknown <anonvtrede@gmail.com>
2021-11-14 13:55:45 +0300

    lesson 100, refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,43 +1,43 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
 
-def index
- @articles = Article.all
-end
+  def index
+    @articles = Article.all
+  end
 
 
   def new 
-      @article = Article.new
+    @article = Article.new
   end
 
-def edit
-  @article = Article.find(params[:id])
-end
+  def edit
 
-def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
-  redirect_to articles_path
-end
-
-def update  
-  @article = Article.find(params[:id])
-  if @article.update(params.require(:article).permit(:title, :description))
-    flash[:notice] = "Article was successfully updated"
-    redirect_to @article
-  else
-    render 'edit'
   end
-end
+
+  def destroy
+
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def update  
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save 
       flash[:notice] = "Article was successfully created"
       redirect_to @article
@@ -46,6 +46,12 @@ end
     end
   end
    
+  private def article_params
+    params.require(:article).permit(:title, :description)
+end
 
-    
+  private def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
