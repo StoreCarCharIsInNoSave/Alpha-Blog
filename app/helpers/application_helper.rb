2754d41c7a9b1532ecca3a1207a4cd4d8eb22a23
unknown <anonvtrede@gmail.com>
2021-11-20 22:17:49 +0300

    Add profile image (using gravatar)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+    def gravatar_for(user, options = { size: 80 })
+        email_adress = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_adress)
+        gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{options[:size]}"
+        image_tag(gravatar_url, alt: user.username, class: 'rounded shadow mx-auto d-block')
+    end
 end
