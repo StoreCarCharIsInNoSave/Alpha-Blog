e5a7acb49a53c7b12d2aa9a2668c2daca16161a2
unknown <anonvtrede@gmail.com>
2021-11-24 00:34:40 +0300

    Added category table and unuseless test for it

---
+++ test/models/category_test.rb
@@ -0,0 +1,9 @@
+#unit test carugory
+require 'test_helper'
+class CategoryTest < ActiveSupport::TestCase
+    test "category should be valid" do
+        category = Category.new(name: "sports")
+        assert category.valid?
+    end
+    
+end
