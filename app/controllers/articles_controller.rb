4f56d1f2bd270e11c93ecfdea3bed40752db8963
unknown <anonvtrede@gmail.com>
2021-11-26 15:58:32 +0300

    Created ManyToMany with Category-articles, added category system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,18 +2,17 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :require_user, except: [:show, :index]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
 
   end
 
-
   def index
-    
+
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
-
-  def new 
+  def new
     @article = Article.new
   end
 
@@ -27,10 +26,10 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  def update  
+  def update
 
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:notice] = 'Article was successfully updated'
       redirect_to @article
     else
       render 'edit'
@@ -40,25 +39,27 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user = current_user
-    if @article.save 
-      flash[:notice] = "Article was successfully created"
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
       redirect_to @article
-    else 
+    else
       render 'new'
     end
   end
-   
-  private def article_params
-    params.require(:article).permit(:title, :description)
-end
 
-  private def set_article
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
+  end
+
+  def set_article
     @article = Article.find(params[:id])
   end
 
   def require_same_user
     if current_user != @article.user and !current_user.admin?
-      flash[:danger] = "You can only edit or delete your own articles"
+      flash[:danger] = 'You can only edit or delete your own articles'
       redirect_to root_path
     end
   end
